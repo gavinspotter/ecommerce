ec12ad30de897a4fddcfeeb4900933e63c19273a
Osiris <potterosiris@gmail.com>
2021-01-29 16:50:53 -0500

    hs imp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {Container} from "react-bootstrap"
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from "./screens/HomeScreen"
 
 const App = () => {
   return (
