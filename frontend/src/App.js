eeaec71d6ecc436fafccac26eca6419fadcc1a0f
Osiris <potterosiris@gmail.com>
2021-01-29 13:45:22 -0500

    imp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
+import {Container} from "react-bootstrap"
 import './App.css';
 
 const App = () => {
   return (
    
     <> 
-
+  <h2>pro shop</h2>
     </>
   );
 }
