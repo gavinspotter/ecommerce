65a7ad93facbdf22ec26a0b914b885a77dad2df5
Osiris <potterosiris@gmail.com>
2021-01-29 16:55:12 -0500

    card

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Card } from 'react-bootstrap'
 
 const Product = () => {
     return (
-        <div>
-            
-        </div>
+        <Card className="my-3 p-3 rounded">
+
+        </Card>
     )
 }
 
