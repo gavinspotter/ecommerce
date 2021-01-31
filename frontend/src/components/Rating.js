6e8cdc3987d419f8c79f2835cc04f70ebd5b259c
Osiris <potterosiris@gmail.com>
2021-01-31 15:04:08 -0500

    prop types

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import propTypes from 'prop-types'
+import PropTypes from 'prop-types'
 
 const Rating = ({value, text, color}) => {
     return (
@@ -36,6 +36,12 @@ const Rating = ({value, text, color}) => {
 
 Rating.defaultProps= {
     color: '#f8e825'
+}
+
+Rating.propTypes = {
+    value: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    color: PropTypes.string
 }
 
 
