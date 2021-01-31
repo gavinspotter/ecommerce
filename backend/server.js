2c19ae4e0651ad60dfd1390b18c2753db397d5bc
Osiris <potterosiris@gmail.com>
2021-01-31 17:35:03 -0500

    testing

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,8 @@ const express = require("express")
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send("api is running")
+})
+
 app.listen(5000, console.log('server running on port 5000'))
