709c4f185335bc901ae42c985ef290b270f41c77
Osiris <potterosiris@gmail.com>
2021-01-31 16:46:12 -0500

    link

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -6,9 +6,9 @@ import Rating from "./Rating"
 const Product = ({ product }) => {
     return (
         <Card className="my-3 p-3 rounded">
-            <a href={`/product/${product._id}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Img src={product.image} variant="top" />
-            </a>
+            </Link>
             <Card.Body>
                 <a href={`/product/${product._id}`}>
                     <Card.Title as="div">
