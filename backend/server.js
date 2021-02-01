79f01267ad1cdd846616fede8b68223561fd08ad
Osiris <potterosiris@gmail.com>
2021-02-01 11:15:29 -0500

    type module

--- backend/server.js
+++ backend/server.js
@@ -21,4 +21,4 @@ app.get('/api/products/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log('server running on port 5000'))
+app.listen(PORT, console.log(`server running on port ${PORT}`))
